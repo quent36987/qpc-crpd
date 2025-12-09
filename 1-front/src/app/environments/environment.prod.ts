65e26bf575bcd7d29ca93ac9b32af481d826c1d5
qgoujon <quentin.goujon@epita.fr>
2025-12-09 22:26:13 +0100

    ngninx

--- 1-front/src/app/environments/environment.prod.ts
+++ 1-front/src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-export const API_URL =  'https://xxxxxxxxxx.fr/api';
-export const API_URL_OPENAPI ='https://xxxxxxxxxxxx';
+export const API_URL =  'https://api.qpc.quentin.ovh/api';
+export const API_URL_OPENAPI ='https://api.qpc.quentin.ovh';
 export const TOKEN_TIMEOUT = 1000 * 60 * 60 * 24 * 2; // 2 jours
 
 
